2e3ed43f4b278497332a4a6c7f9a384abf4a8239
jpasteur-iut90 <ton-email@github.com>
2025-10-15 14:54:54 +0200

    j'ai caca

--- drmad/src/App.vue
+++ drmad/src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+   <h1>Welcome to DrMad app</h1>
+    <router-view></router-view>
 </template>
+
+<script setup>
+import {onMounted} from "vue";
+import {useShopStore} from "@/stores/shop.js";
+
+const shopStore = useShopStore()
+
+// Dès que l'appli est lancée, on va chercher la liste des virus pour la stocker dans le store
+onMounted(() => {
+  shopStore.getAllViruses()
+})
+</script>
 
 <style scoped></style>
---
+++ drmad/src/views/BankAccountView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Account amount</h1>
+
+    <span>account number</span><input v-model="number">
+    <button @click="bankStore.getAccountAmount(number)">Get amount</button><button @click="bankStore.getAccountTransactions(number)">Get transactions</button>
+    <p>available amount: {{bankStore.accountAmount}}</p>
+    <hr />
+    <p>passed transactions: {{ bankStore.accountTransactions }}</p>
+
+    <ul>
+      <li v-for="i in 188">
+        <p>{{ bankStore.getAccountAmount() }}</p>
+        <p>{{  }} </p>
+      </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+import {useBankStore} from "@/stores/bank.js";
+
+const number = ref('')
+const bankStore = useBankStore()
+const transaction = bankStore.getAccountTransactions
+</script>
